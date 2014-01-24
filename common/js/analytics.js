366626f94e6b4ad94eae57842b83d77118edc1c1
gedatsu <nobuhiro.osada@facebook.com>
2014-01-24 17:25:52 +0900

    change analytics profile

--- common/js/analytics.js
+++ common/js/analytics.js
@@ -3,5 +3,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-47117536-2', 'fc2.com');
+  ga('create', 'UA-47117536-3', 'gedatsu.github.io');
   ga('send', 'pageview');
